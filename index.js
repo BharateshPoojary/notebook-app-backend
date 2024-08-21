d836e8924b5385b21cf8c9560b187a32165d6237
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-08-21 08:40:30 +0530

    Cors implemented

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import connectToMongo from './dbconnect.js';
 import express from 'express';
 import auth from './routes/auth.js';
 import notes from './routes/notes.js';
-
+import cors from 'cors';
 (async () => {
     await connectToMongo();
 })();//connecting to mongodb
+
 const app = express();//creating instance of express
 const port = "5000";
+app.use(cors());
 app.use(express.json());//express.json()is a built in middleware function in Express for working with request body
 app.use('/api/auth', auth);//app.use is used to define a middleware function and here I am redirecting the request to a seperate file
 app.use('/api/notes', notes);
