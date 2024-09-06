218b1cef5b4c07dac894c4201b549f41145849ac
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-06 12:29:07 +0530

    updated

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import express from 'express';
 import auth from './routes/auth.js';
 import notes from './routes/notes.js';
 import cors from 'cors';
+import serverless from 'serverless-http';
 (async () => {
     await connectToMongo();
 })();//connecting to mongodb
 
 const app = express();//creating instance of express
-const port = "5000";
+// const port = "5000";
 app.use(cors());
 app.use(express.json());//express.json()is a built in middleware function in Express for working with request body
 app.use('/api/auth', auth);//app.use is used to define a middleware function and here I am redirecting the request to a seperate file
@@ -20,3 +21,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 })
+// Export the serverless function handler
+module.exports.handler = serverless(app);
