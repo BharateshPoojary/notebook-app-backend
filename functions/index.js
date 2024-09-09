fcdad219cd4a9f7ca06c34570ef4b07efc3c2738
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-09 22:20:09 +0530

    done

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ import serverless from 'serverless-http';
 const app = express();//creating instance of express
 const port = "5000";
 app.use(cors());
-app.use(express.json());//express.json()is a built in middleware function in Express for working with request body
+app.use(express.json());//express.json() is a built in middleware function in Express for working with request body
 app.use('/api/auth', auth);//app.use is used to define a middleware function and here I am redirecting the request to a seperate file
 app.use('/api/notes', notes);
 
