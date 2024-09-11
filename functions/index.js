421dbc6bffc980a34d3332fe5acfcf6cfdf05f82
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-11 16:37:45 +0530

    issue fixed

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ app.use('/api/auth', auth);//app.use is used to define a middleware function and
 app.use('/api/notes', notes);
 
 app.get('/', (req, res) => {
-    res.json({ "greeting": "Hello from Bharat" });
+    res.json({ message: "Hello from Bharat" });
 });
 // app.listen(port, () => {
 //     console.log(`http://localhost:${port}`);
