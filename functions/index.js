39df8f775425fb27839abf1a858a08e926346c9c
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-12 10:49:48 +0530

    issue fixed

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,47 @@
-import connectToMongo from '../dbconnect.js';
-import express from 'express';
-import auth from '../routes/auth.js';
-import notes from '../routes/notes.js';
-import cors from 'cors';
-import serverless from 'serverless-http';
-(async () => {
-    try {
-        await connectToMongo();
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB', error);
-    }
-})();//connecting to mongodb
+// import connectToMongo from '../dbconnect.js';
+// import express from 'express';
+// import auth from '../routes/auth.js';
+// import notes from '../routes/notes.js';
+// import cors from 'cors';
+// import serverless from 'serverless-http';
+// (async () => {
+//     try {
+//         await connectToMongo();
+//         console.log('Connected to MongoDB');
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB', error);
+//     }
+// })();//connecting to mongodb
 
-const app = express();//creating instance of express
-// const port = "5000";
-app.use(cors());
-app.use(express.json());//express.json() is a built in middleware function in Express for working with request body
-app.use('/api/auth', auth);//app.use is used to define a middleware function and here I am redirecting the request to a seperate file
-app.use('/api/notes', notes);
+// const app = express();//creating instance of express
+// // const port = "5000";
+// app.use(cors());
+// app.use(express.json());//express.json() is a built in middleware function in Express for working with request body
+// app.use('/api/auth', auth);//app.use is used to define a middleware function and here I am redirecting the request to a seperate file
+// app.use('/api/notes', notes);
+
+// app.get('/', (req, res) => {
+//     try {
+
+//         res.json({ message: "Hello from Bharat" });
+//     } catch (error) {
+//         res.status(500).json({ error: "Server error" });
+//     }
+// });
+
+// // app.listen(port, () => {
+// //     console.log(`http://localhost:${port}`);
+// // })
+// // Export the serverless function handler
+// export const handler = serverless(app);//This is for netlify serverless function
+// export default app
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
 
 app.get('/', (req, res) => {
-    try {
-
-        res.json({ message: "Hello from Bharat" });
-    } catch (error) {
-        res.status(500).json({ error: "Server error" });
-    }
+    res.json({ message: 'Hello, bharat' });
 });
 
-// app.listen(port, () => {
-//     console.log(`http://localhost:${port}`);
-// })
-// Export the serverless function handler
-export const handler = serverless(app);//This is for netlify serverless function
+module.exports.handler = serverless(app);
