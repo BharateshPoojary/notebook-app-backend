04cc0826f4f41a7dce80c14ad615a2d128305a03
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-12 09:21:22 +0530

    issue fixed

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,11 @@
-import connectToMongo from '../dbconnect.js';
-import express from 'express';
-import auth from '../routes/auth.js';
-import notes from '../routes/notes.js';
-import cors from 'cors';
-import serverless from 'serverless-http';
-(async () => {
-    try {
-        await connectToMongo();
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB', error);
-    }
-})();//connecting to mongodb
+// 
+const express = require('express');
+const serverless = require('serverless-http');
 
-const app = express();//creating instance of express
-// const port = "5000";
-app.use(cors());
-app.use(express.json());//express.json() is a built in middleware function in Express for working with request body
-app.use('/api/auth', auth);//app.use is used to define a middleware function and here I am redirecting the request to a seperate file
-app.use('/api/notes', notes);
+const app = express();
 
 app.get('/', (req, res) => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Hello from Bharat" }),
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: "Server Error" }),
-        };
-    }
+    res.send('Hello, World!');
 });
 
-// app.listen(port, () => {
-//     console.log(`http://localhost:${port}`);
-// })
-// Export the serverless function handler
-export default serverless(app);//This is for netlify serverless function
-// export default app
+module.exports.handler = serverless(app);
